0d8717e8586f29622cfef5192231ca25fea068e5
Abhay Vachhani <84565477+Abhay-Vachhani@users.noreply.github.com>
2025-02-18 06:14:26 +0000

    Example of an call HF API

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
+from gradio_client import Client
 
+client = Client("abhayv28/MISTRAL-0.3-7B")
+result = client.predict(
+		prompt="Hello!!",
+		system_prompt=None,
+		api_name="/generate"
+)
+print(result)
